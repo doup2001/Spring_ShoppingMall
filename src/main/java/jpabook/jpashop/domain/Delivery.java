3cd5f90a71a21c6b655e30f8adb1575a7bdcc598
eedo_y <doup2001@naver.com>
2024-08-05 23:27:18 +0900

    Add: Domain Package

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Delivery {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "delivery_id")
+    private long id;
+
+    @Embedded
+    private Address address;
+
+    @OneToOne(mappedBy = "delivery",fetch = FetchType.LAZY)
+    private Order order;
+
+    @Enumerated(EnumType.ORDINAL)
+    private DeliveryStatus deliveryStatus;
+
+
+}
