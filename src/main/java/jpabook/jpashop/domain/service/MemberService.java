f050eafd7deae03cc0da837944d2adbd63a8e73b
eedo_y <doup2001@naver.com>
2024-08-09 16:52:05 +0900

    Add : 회원가입 기능 추가

--- src/main/java/jpabook/jpashop/domain/service/MemberService.java
+++ src/main/java/jpabook/jpashop/domain/service/MemberService.java
@@ -1,12 +1,16 @@
 package jpabook.jpashop.domain.service;
 
+import jakarta.transaction.Transactional;
 import jpabook.jpashop.domain.entity.Member;
 import jpabook.jpashop.domain.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -15,4 +19,14 @@ public class MemberService {
         Long memberId = memberRepository.save(member);
         return memberId;
     }
+
+    public Member findByone(Long id) {
+        Member member = memberRepository.findByOne(id);
+        return member;
+    }
+
+    public List<Member> findByALl() {
+        return memberRepository.findByAll();
+    }
+
 }
