d067dfd9bce7d29a9fb1d41ac60978b9fe98ab83
eedo_y <doup2001@naver.com>
2024-08-13 23:36:45 +0900

    add: 검색 기능 완료

--- src/main/java/jpabook/jpashop/domain/controller/OrderController.java
+++ src/main/java/jpabook/jpashop/domain/controller/OrderController.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.controller;
 
 
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.domain.entity.Item.Item;
 import jpabook.jpashop.domain.entity.Member;
 import jpabook.jpashop.domain.entity.Order;
@@ -8,6 +9,7 @@ import jpabook.jpashop.domain.service.ItemService;
 import jpabook.jpashop.domain.service.MemberService;
 import jpabook.jpashop.domain.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +18,7 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class OrderController {
 
     private final MemberService memberService;
@@ -35,7 +38,7 @@ public class OrderController {
     }
 
     @PostMapping("/order")
-    public String order(@RequestParam Long memberId, @RequestParam Long itemId, @RequestParam int count,Model model) {
+    public String order(@RequestParam Long memberId, @RequestParam Long itemId, @RequestParam int count, Model model) {
 
         Long orderId = orderService.order(memberId, itemId, count);
         Order order = orderService.findById(orderId);
@@ -53,4 +56,21 @@ public class OrderController {
 
         return "order/orderList";
     }
+
+    //주문취소 버튼
+    @PostMapping("/orders/{orderId}/cancel")
+    public String cancel(@PathVariable Long orderId) {
+
+        Order order = orderService.findById(orderId);
+        orderService.cancelOrder(order.getId());
+
+        return "redirect:/orders";
+    }
+
+    //주문 검색 버튼
+    @PostMapping("/orders")
+    public String search(@RequestParam String memberName, @RequestParam OrderStatus orderStatus) {
+        return "redirect:/orders";
+    }
+
 }
